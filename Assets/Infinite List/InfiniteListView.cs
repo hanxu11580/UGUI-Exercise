679f25396a82e1107bd80a929f6ead467d39915f
Xuhan <626150455@qq.com>
2021-09-13 18:31:34 +0800

    2021.09.13 - 无限滚动任务列表(未完成)-加一点东西

--- Assets/Infinite List/InfiniteListView.cs
+++ Assets/Infinite List/InfiniteListView.cs
@@ -8,15 +8,19 @@ public class InfiniteListView : MonoBehaviour
 {
     [SerializeField] Transform _content;
     ScrollRect _scrollRect;
+    RectTransform _selfRect;
+    Vector3[] _windowCorner;
 
     List<TaskModel> _taskModels;
     //Task Item
     RectTransform[] _itemRectTrans;
     TextMeshProUGUI[] _itemTmpTexts;
+    Vector3[] _itemCorner;
     //End
     private void Start()
     {
         _scrollRect = GetComponent<ScrollRect>();
+        _selfRect = GetComponent<RectTransform>();
         _itemRectTrans = new RectTransform[_content.childCount];
         _itemTmpTexts = new TextMeshProUGUI[_content.childCount];
         LoadTaskData();
@@ -24,16 +28,26 @@ public class InfiniteListView : MonoBehaviour
         {
             Transform t = _content.GetChild(i);
             _itemRectTrans[i] = t.GetComponent<RectTransform>();
-            Debug.Log(t.GetChild(0).name);
             _itemTmpTexts[i] = t.GetComponentInChildren<TextMeshProUGUI>();
             // 初始化Task Item UI
             _itemTmpTexts[i].text = _taskModels[i].textInfo;
         }
+        _itemCorner = new Vector3[4];
+        _windowCorner = new Vector3[4];
+        _selfRect.GetWorldCorners(_windowCorner);
     }
 
     private void Update()
     {
-        
+        foreach (var item in _itemRectTrans)
+        {
+            item.GetWorldCorners(_itemCorner);
+            // 框上面的
+            if(_itemCorner[0].y > _windowCorner[1].y)
+            {
+                item.gameObject.SetActive(false);
+            }
+        }
     }
 
 
